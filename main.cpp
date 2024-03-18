388d1e374dd240412f80fdc92cfca59650789032
Mădălin <madalin.andrei.ioana@gmail.com>
2024-03-18 21:01:27 +0200

    Added files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "CharStack.h"
+
+int main() {
+    CharStack stiva;
+//    stiva.Push('t');
+//    stiva.Push('s');
+//    stiva.Push('e');
+//    stiva.Push('t');
+    std::cin >> stiva;
+    std::cout << stiva;
+
+    return 0;
+}
